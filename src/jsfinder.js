1611ba7d67507bd1821ee0bde4e86e8c63c5bdbf
Henri Ylikotila <kelesti@gmail.com>
2012-09-24 15:42:38 +0300

    filtering non js files out of the result set

--- src/jsfinder.js
+++ src/jsfinder.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
-//var files = fs.readdirSync(__dirname + '/../examples/');
 var files = require('findit').sync(__dirname);
-console.dir(files);
+
 
 var jsArray = new Array();
 var jsArrayCount = 0;
@@ -10,14 +9,11 @@ function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
-/*
 for (var i=0; i<files.length; i++) {
 	if(endsWith(files[i],'.js')) {
 		jsArray[jsArrayCount] = __dirname + files[i];
-		//console.log('file: ' + files[i] + '\n');
 		jsArrayCount++;
 	}
 }
-*/
 
-//console.log(jsArray);
+console.log(jsArray);
