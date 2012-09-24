119cb1c704a76f38c0c2250e896c34b47d959fb1
Henri Ylikotila <kelesti@gmail.com>
2012-09-24 16:05:03 +0300

    made the function to also work without a given commandline parameter

--- src/jsfinder.js
+++ src/jsfinder.js
@@ -1,8 +1,16 @@
-var fs = require('fs');
-var files = require('findit').sync(__dirname + process.argv[2]);
-
+var userInput = '/';
 var jsArray = new Array();
 var jsArrayCount = 0;
+
+if(process.argv.length < 3) {
+	console.log(process.argv.length);
+	console.log('No input parameter given. Beginning search from current folder. \n');
+} else {
+	userInput = process.argv[2];
+}
+
+var files = require('findit').sync(__dirname + userInput);
+
 
 function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
