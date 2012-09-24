6f80b1ed8f096cdbe2653ae84e5682dc3d385fca
Henri Ylikotila <kelesti@gmail.com>
2012-09-24 16:13:02 +0300

    added some initial comments to the functions

--- src/jsfinder.js
+++ src/jsfinder.js
@@ -2,6 +2,7 @@ var userInput = '/';
 var jsArray = new Array();
 var jsArrayCount = 0;
 
+// Checking if the user gave a target folder as a parameter.
 if(process.argv.length < 3) {
 	console.log(process.argv.length);
 	console.log('No input parameter given. Beginning search from current folder. \n');
@@ -9,13 +10,15 @@ if(process.argv.length < 3) {
 	userInput = process.argv[2];
 }
 
+// Walking through the folder recursively and saving the files found to a list.
 var files = require('findit').sync(__dirname + userInput);
 
-
+// A function for checking if parameter str ends with the suffix parameter.
 function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
+// Iterating through the files to find all the .js files and adding those to an array.
 for (var i=0; i<files.length; i++) {
 	if(endsWith(files[i],'.js')) {
 		jsArray[jsArrayCount] = __dirname + files[i];
@@ -23,5 +26,5 @@ for (var i=0; i<files.length; i++) {
 	}
 }
 
-
+// Printing out the array for debugging purposes.
 console.log(jsArray);
