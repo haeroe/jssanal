58bd23a67cdae6e59545b4866d4b89a4bc74360a
Henri Ylikotila <kelesti@gmail.com>
2012-09-24 16:14:53 +0300

    added some more commentary

--- src/jsfinder.js
+++ src/jsfinder.js
@@ -13,12 +13,13 @@ if(process.argv.length < 3) {
 // Walking through the folder recursively and saving the files found to a list.
 var files = require('findit').sync(__dirname + userInput);
 
-// A function for checking if parameter str ends with the suffix parameter.
+// A function for checking if string str ends with the suffix string.
 function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
 // Iterating through the files to find all the .js files and adding those to an array.
+// Here we are assuming that the path is relative to the current folder.
 for (var i=0; i<files.length; i++) {
 	if(endsWith(files[i],'.js')) {
 		jsArray[jsArrayCount] = __dirname + files[i];
