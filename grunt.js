7ecec39eb005be01e52ea0866126d1471021bf0a
Henri Ylikotila <kelesti@gmail.com>
2012-09-26 13:03:57 +0300

    grunt lint jshint configured more precisely

--- grunt.js
+++ grunt.js
@@ -1,9 +1,34 @@
-module.exports = function(grunt) {
+#!/usr/bin/env node
 
-grunt.initConfig({
-	lint: {
-		all: ['src/*.js', 'test/*.js', 'grunt.js']
-	}
-}); 
+module.exports = function (grunt) {
+    "use strict";
 
+    // Project configuration.
+    grunt.initConfig({
+        lint: {
+            all: ["grunt.js", "src/*.js", "test/*.js"]
+        },
+        jshint: {
+            options: {
+                browser: true,
+                curly: true,
+                eqeqeq: true,
+                newcap: true,
+                undef: true,
+                eqnull: true,
+                node: true
+            },
+            globals: {
+                exports: true,
+                Ext: false,
+                console: false,
+                alert: false,
+                prompt: false
+            }
+        }
+    });
+
+    // Default task.
+    grunt.registerTask('default', 'lint');
 };
+
