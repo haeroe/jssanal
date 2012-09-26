ee9a4e9d48778892fc3f4250c41bad8bac38905f
katjakk <katjakk@saunalahti.fi>
2012-09-26 17:32:23 +0300

    Added qunit to package.json and tests directory.

---
+++ tests/testrunner.js
@@ -0,0 +1,29 @@
+var testrunner = require("qunit");
+
+try {
+  process.chdir(__dirname);
+  console.log('New work directory: ' + process.cwd());
+}
+catch (err) {
+  console.log('chdir: ' + err);
+}
+
+testrunner.setup({
+  log: {
+    summary: true
+    assertions: true,
+    errors: true,
+    tests: true,
+    summary: true,
+    globalSummary: true,
+    testing: true
+  },
+  coverage: false,
+  deps: null,
+  namespace: null
+});
+
+testrunner.run({
+    code : "../examples/main.js",
+    tests : "./tests.js"
+});
---
+++ tests/tests.js
@@ -0,0 +1,9 @@
+test("Hello World", function() {
+    ok(true);
+});
+
+test("a basic test example", function (assert) {
+    ok(true, "this test is fine");
+    var value = "hello";
+    equal("hello", value, "We expect value to be hello");
+});
