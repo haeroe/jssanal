ad60c03205a2b137786b43f758c01ae661c5e492
Henri Ylikotila <kelesti@gmail.com>
2012-10-01 14:25:57 +0300

    fixed tests folder

--- grunt.js
+++ grunt.js
@@ -1,37 +1,40 @@
 #!/usr/bin/env node
 
 module.exports = function (grunt) {
-    "use strict";
+"use strict";
 
-    // Project configuration.
-    grunt.initConfig({
-        lint: {
-            all: ["grunt.js", "src/*.js", "test/*.js"]
-        },
-        jshint: {
-            options: {
-                browser: true,
-                curly: true,
-                eqeqeq: true,
-                newcap: true,
-                undef: true,
-                eqnull: true,
-                node: true
-            },
-            globals: {
-                exports: true,
-                Ext: false,
-                console: false,
-                alert: false,
-                prompt: false
-            }
-        },
-        qunit: {
-        		index: ['test/*.js']
-        }
-    });
+// Project configuration.
+grunt.initConfig({
+	lint: {
+		all: ["grunt.js", "src/*.js", "tests/*.js"]
+	},
+	jshint: {
+		options: {
+			browser: true,
+			curly: true,
+			eqeqeq: true,
+			newcap: true,
+			undef: true,
+			eqnull: true,
+			node: true
+		},
+		globals: {
+			exports: true,
+			Ext: false,
+			console: false,
+			alert: false,
+			prompt: false
+		}
+	},
+	qunit: {
+		index: ['tests/tests.js']
+		}
+	});
 
-    // Default task.
-    grunt.registerTask('default', 'lint', 'qunit');
+	// Default task.
+	grunt.registerTask('default', 'lint', 'qunit');
+
+	// Travis CI task.
+	grunt.registerTask('travis', 'lint qunit');
 };
 
