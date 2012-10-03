df867f93c0d58bb8d7a4723a6aadab352be6e652
Santtu Keskinen <laquendi@gmail.com>
2012-10-03 13:12:20 +0300

    Testing travis with a new branch.

--- src/main.js
+++ src/main.js
@@ -7,5 +7,5 @@ var file = fs.readFileSync( file_name, 'ascii' );
 
 var syntax_tree = esprima.parse( file, {loc: true, range: true, raw: true, token: true} );
 
-console.log(util.inspect( syntax_tree.body, false, null));
+//console.log(util.inspect( syntax_tree.body, false, null));
 
