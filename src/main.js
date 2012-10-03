5d82468608af066adaa3a53f9e8f8cee3fa00069
Haeroe <lindert@cs.helsinki.fi>
2012-10-03 13:10:20 +0200

    Started working through the syntax tree. Started adding specific cases for different node types and maintaining some kind of state information to help track sources/sinks.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,89 @@
-var esprima = require('esprima'),
-	fs = require('fs'),
-	util = require('util');
+var esprima 	= require('esprima');
+var fs		= require('fs');
+var util	= require('util');
 
-var file_name = process.argv[2];
-var file = fs.readFileSync( file_name, 'ascii' );
+var methods	= {};
+var variables	= {};
 
-var syntax_tree = esprima.parse( file, {loc: true, range: true, raw: true, token: true} );
+var currMethod;
+var currVar;
 
-console.log(util.inspect( syntax_tree.body, false, null));
 
+function newMethod(name)
+{
+	tempMethod = new Object();
+
+	tempMethod.name = name;
+
+	tempMethod.isSink = false;
+	tempMethod.isSource = false;
+
+	tempMethod.params = {};
+	tempMethod.calls = {};
+
+	tempMethod.addParam = function (paramName, isTainted)
+				{
+					params.paramName = isTainted;
+				}
+
+	tempMethod.addCall = function (callee, isDangerous)
+				{
+					calls.callee = isDangerous;
+				}
+
+	return tempMethod;
+}
+
+function parseAST(astBlock)
+{
+	if(astBlock.type === undefined)
+		return;
+
+	var localMethod = currMethod;
+	var localVar = currVar;
+
+	switch(astBlock.type)
+	{
+		case "CallExpression":
+			var method = newMethod(astBlock.callee.name);
+			
+			break;
+
+
+		case "Program":
+			for(var child in astBlock.body)
+			{
+				parseAST(astBlock.body[child]);
+			}
+			break;
+
+
+		case "VariableDeclaration":
+			for(var child in astBlock.declarations)
+			{
+				parseAST(astBlock.declarations[child]);
+			}
+			break;
+
+
+		case "VariableDeclarator":
+			//TODO: Make astBlock.id.name globally unique and store it
+			parseAST(astBlock.init);
+			break;			
+
+
+		default:
+			console.log("Type not found! - " + astBlock.type);
+			process.exit(1);
+	}
+
+	currMethod = localMethod;
+	currVar = localVar;
+}
+
+
+var file_name 	= process.argv[2];
+var file 	= fs.readFileSync( file_name, 'ascii' );
+var ast		= esprima.parse( file, {loc: true, range: true, raw: true, token: true} );
+
+parseAST(ast);
