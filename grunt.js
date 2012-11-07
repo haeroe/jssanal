dd520a53d2b879f5d632c53b9d3a2f69230eaf4e
Henri Ylikotila <kelesti@gmail.com>
2012-11-07 11:45:06 +0200

    removed jshint

--- grunt.js
+++ grunt.js
@@ -8,24 +8,6 @@ grunt.initConfig({
 	lint: {
 		all: ["grunt.js", "src/*.js"]
 	},
-	jshint: {
-		options: {
-			browser: true,
-			curly: true,
-			eqeqeq: true,
-			newcap: true,
-			undef: true,
-			eqnull: true,
-			node: true
-		},
-		globals: {
-			exports: true,
-			Ext: false,
-			console: false,
-			alert: false,
-			prompt: false
-		}
-	},
 	test: {
 		files: ['tests/exampletest.js']
 	}
