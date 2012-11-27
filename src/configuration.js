3b938975a51a2668a5f97f2a960cfa3347097b47
katjakk <katjakk@saunalahti.fi>
2012-11-27 16:50:22 +0200

    added escape functions to whitelist

--- src/configuration.js
+++ src/configuration.js
@@ -1,5 +1,5 @@
 // for setting your own whitelisted functions that you want to ignore.
-var whiteList = [""];
+var whiteList = ["escape","htmlEscape", "esc"];
 
 // for setting your own blacklisted functions that you want to include in addition to the frameWork defaults.
 var blackList = [""];
@@ -7,52 +7,52 @@ var blackList = [""];
 var functionSinks = ["eval", "Function", "setTimeout", "setInterval", "execScript"];
 
 var memberAssignmentSinks = {
-    "innerHTML" : null,
-    "src" :	null,
-    "text" : null,
-    "textContent" : null,
-    "innerText" : null,
-    "value"	: null,
+    "innerHTML"    : null,
+    "src"          : null,
+    "text"         : null,
+    "textContent"  : null,
+    "innerText"    : null,
+    "value"	       : null,
 
-    "hostname" : ["location"],
-    "protocol" : ["location"],
-    "pathname" : ["location"],
-    "href" : ["location"],
-    "location" : ["window",	"document"]  
+    "hostname"     : ["location"],
+    "protocol"     : ["location"],
+    "pathname"     : ["location"],
+    "href"         : ["location"],
+    "location"     : ["window",	"document"]  
 };
 
 var memberFunctionSinks	= {
     "generateCRMFRequest" : ["crypto"],
-    "write"   : ["document"],
-    "writeln" : ["document"],
+    "write"        : ["document"],
+    "writeln"      : ["document"],
     "createContextualFragment" : ["Range"],
-    "search"  : ["location"],
-    "assign"  : ["location"],
-    "replace" : ["location"],
-    "html"       : [null], // jQuery MemberExpression 
-    "append"     : [null], // jQuery MemberExpression  
-    "parseHTML"  : [null], // jQuery MemberExpression
-    "globalEval" : [null], // jQuery MemberExpression
+    "search"       : ["location"],
+    "assign"       : ["location"],
+    "replace"      : ["location"],
+    "html"         : [null], // jQuery MemberExpression 
+    "append"       : [null], // jQuery MemberExpression  
+    "parseHTML"    : [null], // jQuery MemberExpression
+    "globalEval"   : [null], // jQuery MemberExpression
 };
 
 var varSources = ["location"];
 
 var memberVarSources = {
-	"referrer" : ["document"],
-	"URL" : ["document"],
-	"documentURI" : ["document"],
+	"referrer"     : ["document"],
+	"URL"          : ["document"],
+	"documentURI"  : ["document"],
 	"URLUnencoded" : ["document"],
-	"baseURI" : ["document"],
-	"cookie" : ["document"],
-	"href" : ["location"],
-	"search" : ["location"],
-	"hash" : ["location"],
-	"pathname" : ["location"],
-	"name" : ["window"]
+	"baseURI"      : ["document"],
+	"cookie"       : ["document"],
+	"href"         : ["location"],
+	"search"       : ["location"],
+	"hash"         : ["location"],
+	"pathname"     : ["location"],
+	"name"         : ["window"]
 };
 
 var memberFunctionSources = {
-	"pushState" : ["history"],
+	"pushState"    : ["history"],
 	"replaceState" : ["history"]
 };
 
