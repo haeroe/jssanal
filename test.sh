0afb3d4801da30cac8109a54d596cfd7061efbcb
katjakk <katjakk@saunalahti.fi>
2012-12-12 12:25:02 +0200

    added failure status to test script

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-grunt test | grep "\(^---\|WARN\|>> [^A][^s][^s][^e][^r][^t][^i][^o][^n].* - \)"
+#grunt test | grep '\(^---\|WARN\|>> [^A][^s][^s][^e][^r][^t][^i][^o][^n].* - \)' | sed -n '/_TEST/p' | sed 's/>> /FAILED: /g'
+grunt test | grep '\(^---\|WARN\|>> [^A][^s][^s][^e][^r][^t][^i][^o][^n].* - \)' | sed -n '/_TEST\|WARN/p' | sed 's/>> /FAILED: /g'
